089ce3066f8f4e60ae56949d13fc46a30c674548
mwang20 <mwang20@stuy.edu>
2020-10-26 11:00:07 -0400

    I used array B to test the largest method, which is supposed to print out 5, and it did.

--- Tester.java
+++ Tester.java
@@ -3,5 +3,7 @@ public class Tester{
     int[][] A = {{ 1, 0, 12, -1 }, {7, -2, 2, 1}, {-5, -2, 2, -9}};
     int[] B = {1, 3, 5};
     System.out.println(ArrayOps.sum(B));
+    System.out.println();
+    System.out.println(ArrayOps.largest(B));
   }
 }
