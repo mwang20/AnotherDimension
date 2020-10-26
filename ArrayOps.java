dc71d3d7ce44cc3d65105f7a7f28e1a0105f95bf
mwang20 <mwang20@stuy.edu>
2020-10-26 10:52:00 -0400

    I completed the largest method. I have done this method before so I think it is fine.

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,13 @@ public class ArrayOps{
     }
     return sumArray;
   }
+  public static int largest(int[] arr){
+    int largestArray = 0;
+    for (int i = 0; i < arr.length; i ++){
+      if (arr[i] > largestArray){
+        largestArray = arr[i];
+      }
+    }
+    return largestArray;
+  }
 }
