75d648041dd58098f1bb8943394d9d88b553d0b3
mwang20 <mwang20@stuy.edu>
2020-10-26 21:33:02 -0400

    The 2D array sum method was very easy to make because it was just the sumRows method except you return sum(newRows) instead of newRows

--- ArrayOps.java
+++ ArrayOps.java
@@ -33,10 +33,21 @@ public class ArrayOps{
       for ( int j = 0; j < matrix[i].length; j++){
         if (matrix[i][j] > largestInt){
           largestInt = matrix[i][j];
-      }
+        }
       }
       newRows[i] = largestInt;
     }
     return newRows;
+  }
+  public static int sum(int[][] arr){
+    int[] newRows = new int[arr.length];
+    for ( int i = 0; i < arr.length; i++){
+      int sum = 0;
+      for ( int j = 0; j < arr[i].length; j++){
+        sum = sum + arr[i][j];
+      }
+      newRows[i] = sum;
+    }
+    return sum(newRows);
   }
 }
