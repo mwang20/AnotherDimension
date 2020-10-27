4979835a4341b6d1adc95d86ff849552b8a5e910
mwang20 <mwang20@stuy.edu>
2020-10-27 00:27:12 -0400

    I tested the isColMagic method with the array given in the Google Classroom assignment info, and it printed true as expected, which means the method is working as intended

--- Tester.java
+++ Tester.java
@@ -4,6 +4,7 @@ public class Tester{
     int[][] A = {{ 1, 0, 12, -1 }, {7, -2, 2, 1}, {-5, -2, 2, -9}};
     int[] B = {1, 3, 5};
     int[][] rowMagicTest = {{  1,  2, 3, 4 }, {  2, 3,  4,  1 }, { 3, 4,  1, 2 }};
+    int[][] colMagicTest = {{  1,  1, 1 }, {2, 2, 2 }, {3, 3, 3}};
     System.out.println(ArrayOps.sum(B));
     System.out.println();
     System.out.println(ArrayOps.largest(B));
@@ -17,5 +18,7 @@ public class Tester{
     System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
     System.out.println();
     System.out.println(ArrayOps.isRowMagic(rowMagicTest));
+    System.out.println();
+    System.out.println(ArrayOps.isColMagic(colMagicTest));
   }
 }
