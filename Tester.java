e25d7368a26cba32570d64d5822fc4b8d5602a2c
mwang20 <mwang20@stuy.edu>
2020-10-26 21:34:44 -0400

    I tested all the part 4 functions with arrays A and B like the Google Classroom assignment said and they all matched the answers on the assignment. Safe to say that they work.

--- Tester.java
+++ Tester.java
@@ -3,14 +3,14 @@ public class Tester{
   public static void main(String[] args){
     int[][] A = {{ 1, 0, 12, -1 }, {7, -2, 2, 1}, {-5, -2, 2, -9}};
     int[] B = {1, 3, 5};
-    int[][] C = {{ 1, 0, 12}, {6, 4, -1}, {7, 5, 4}, {-2, 2, 1}, {-5, 2, -9}};
-
     System.out.println(ArrayOps.sum(B));
     System.out.println();
     System.out.println(ArrayOps.largest(B));
     System.out.println();
-    System.out.println(Arrays.toString(ArrayOps.sumRows(C)));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
     System.out.println();
-    System.out.println(Arrays.toString(ArrayOps.largestInRows(C)));
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+    System.out.println();
+    System.out.println(ArrayOps.sum(A));
   }
 }
